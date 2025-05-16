85e881ebfc3ec723f7b73c96747c45efdd304e0e
BigZorin <zorin@hotmail.nl>
2025-05-16 15:03:06 +0200

    50 cent

--- lib/products.ts
+++ lib/products.ts
@@ -5,7 +5,7 @@ export const products: Product[] = [
     id: "coaching-basic",
     name: "Basis Coaching Pakket",
     description: "Persoonlijke coaching voor beginners",
-    price: 9900, // €99.00
+    price: 50, // €0.50
     features: ["3 coaching sessies", "Persoonlijk actieplan", "Email ondersteuning", "Toegang tot online materialen"],
     metadata: {
       clickfunnels_membership_level: "basic",
